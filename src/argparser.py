c549b75d29b4c52990c3b8337c0437f25f5ce283
김용환 <kim-yonghwan@gim-yonghwan-ui-MacBookPro.local>
2022-12-12 18:11:36 +0900

    add pretrain path at  argparser.py

--- src/argparser.py
+++ src/argparser.py
@@ -29,6 +29,7 @@ def parse_arguments():
     parser.add_argument('--batch-size', default=8, type=int, help='batch size (default: 8)')
     parser.add_argument('--epoch', default=10, type=int, help='total epochs (default: 10)')
     parser.add_argument('--save-path', default='out/', type=str, help='model and log save directory')
+    parser.add_argument('--pretrain-path', default=None, type=str, help='pretrain model path' )
 
     args = parser.parse_args()
     return args
