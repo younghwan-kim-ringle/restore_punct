0f9ea077cb9fab71d5b7ac7157693b91b8ad12c7
김용환 <kim-yonghwan@gim-yonghwan-ui-MacBookPro.local>
2023-01-10 11:32:54 +0900

    add list input in inference

--- src/inference.py
+++ src/inference.py
@@ -13,6 +13,8 @@ parser.add_argument('--lstm-dim', default=-1, type=int,
 parser.add_argument('--use-crf', default=False, type=lambda x: (str(x).lower() == 'true'),
                     help='whether to use CRF layer or not')
 parser.add_argument('--language', default='en', type=str, help='language English (en) oe Bangla (bn)')
+parser.add_argument('--is-list', default=False, type=str, help='if input is list of string')
+parser.add_argument('--list_input', default=[], type=list, help='list of input')
 parser.add_argument('--in-file', default='data/test_en.txt', type=str, help='path to inference file')
 parser.add_argument('--weight-path', default='xlm-roberta-large.pt', type=str, help='model weight path')
 parser.add_argument('--sequence-length', default=256, type=int,
@@ -37,7 +39,8 @@ else:
 deep_punctuation.to(device)
 
 
-def inference():
+def inference1():
+    ## input이 list가 아닌 경우
     deep_punctuation.load_state_dict(torch.load(model_save_path))
     deep_punctuation.eval()
 
@@ -100,6 +103,74 @@ def inference():
     with open(args.out_file, 'w', encoding='utf-8') as f:
         f.write(result)
 
+def inference2():
+    ## input이 list인 경우
+    sents = args.list_input
+    deep_punctuation.load_state_dict(torch.load(model_save_path))
+    deep_punctuation.eval()
+    total_text = ""
+    for text in sents:
+        text = re.sub(r"[,:\-–.!;?]", '', text)
+        words_original_case = text.split()
+        words = text.lower().split()
+        word_pos = 0
+        sequence_len = args.sequence_length
+        result = ""
+        decode_idx = 0
+        punctuation_map = {0: '', 1: ',', 2: '.', 3: '?'}
+        if args.language != 'en':
+            punctuation_map[2] = '।'
+
+        while word_pos < len(words):
+            x = [TOKEN_IDX[token_style]['START_SEQ']]
+            y_mask = [0]
+
+            while len(x) < sequence_len and word_pos < len(words):
+                tokens = tokenizer.tokenize(words[word_pos])
+                if len(tokens) + len(x) >= sequence_len:
+                    break
+                else:
+                    for i in range(len(tokens) - 1):
+                        x.append(tokenizer.convert_tokens_to_ids(tokens[i]))
+                        y_mask.append(0)
+                    x.append(tokenizer.convert_tokens_to_ids(tokens[-1]))
+                    y_mask.append(1)
+                    word_pos += 1
+            x.append(TOKEN_IDX[token_style]['END_SEQ'])
+            y_mask.append(0)
+            if len(x) < sequence_len:
+                x = x + [TOKEN_IDX[token_style]['PAD'] for _ in range(sequence_len - len(x))]
+                y_mask = y_mask + [0 for _ in range(sequence_len - len(y_mask))]
+            attn_mask = [1 if token != TOKEN_IDX[token_style]['PAD'] else 0 for token in x]
+
+            x = torch.tensor(x).reshape(1,-1)
+            y_mask = torch.tensor(y_mask)
+            attn_mask = torch.tensor(attn_mask).reshape(1,-1)
+            x, attn_mask, y_mask = x.to(device), attn_mask.to(device), y_mask.to(device)
+
+            with torch.no_grad():
+                if args.use_crf:
+                    y = torch.zeros(x.shape[0])
+                    y_predict = deep_punctuation(x, attn_mask, y)
+                    y_predict = y_predict.view(-1)
+                else:
+                    y_predict = deep_punctuation(x, attn_mask)
+                    y_predict = y_predict.view(-1, y_predict.shape[2])
+                    y_predict = torch.argmax(y_predict, dim=1).view(-1)
+            for i in range(y_mask.shape[0]):
+                if y_mask[i] == 1:
+                    result += words_original_case[decode_idx] + punctuation_map[y_predict[i].item()] + ' '
+                    decode_idx += 1
+        total_text += result + '\n'
+
+    print('Punctuated text')
+    print(total_text)
+    with open(args.out_file, 'w', encoding='utf-8') as f:
+        f.write(total_text)
+
 
 if __name__ == '__main__':
-    inference()
+    if args.is_list == True:
+        inference2()
+    else:
+        inference1()
